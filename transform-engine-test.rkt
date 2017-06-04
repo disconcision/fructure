6a0e2ea098542a52913e71177d66a19b206b6d97
disconcision <github@disconcision.com>
2017-06-04 01:56:21 -0400

    did transform engine (is slick)

--- transform-engine-test.rkt
+++ transform-engine-test.rkt
@@ -3,12 +3,136 @@
 (require racket/gui/base)
 (require fancy-app)
 
+(define-syntax define/↦
+  (syntax-rules (↦)
+    [(define/↦ transform [pattern ↦ result] ...)
+     (define/match (transform source) [(pattern) result] ...
+       [(_) (if (list? source)
+                (map transform source)
+                source)])]))
 
-; -----------
-; notes for tommorow:
-; forget gui for moment, just do text output
-; manually wrap (single) selector into code tree
-; make and manually test pattern matchers for selector movement
-; make those work recursively
+
+; -------------------------------------------------------
+
+(define (simple-select ls) `(S ,ls))
+
+(define (update src input)
+  (let ([transform (match input
+                     [#\s first-child]
+                     [#\z last-child]
+                     [#\w parent]
+                     [#\a next-sibling]
+                     [#\d prev-sibling]
+                     [#\i delete]
+                     [#\o insert-child-r]
+                     [#\u insert-child-l]
+                     [#\l new-sibling-r]
+                     [#\j new-sibling-l]
+                     [#\k wrap-single])])
+    (transform src)))
+
+(define (loop source stream)
+  (unless (empty? stream)
+    (let* ([input (first stream)]
+           [new-source (update source input)])
+      (map display `(,input " : " ,new-source)) (newline)
+      (loop new-source (rest stream)))))
+
+
+; move selector -----------------------------------------
+
+(define/↦ first-child
+  [`(S (,a ,b ...)) ↦ `((S ,a) ,@b)])
+
+(define/↦ last-child
+  [`(S (,a ... ,b)) ↦ `(,@a (S ,b))])
+
+(define/↦ parent
+  [`(,a ... (S ,b ...) ,c ...) ↦ `(S (,@a ,@b ,@c))])
+
+(define/↦ next-sibling
+  [`(,a ... (S ,b) ,c ,d ...) ↦ `(,@a ,b (S ,c) ,@d)]
+  [`(,a ,b ... (S ,c)) ↦ `((S ,a) ,@b ,c)])
+
+(define/↦ prev-sibling
+  [`(,a ... ,b (S ,c) ,d ...) ↦ `(,@a (S ,b) ,c ,@d)]
+  [`((S ,a) ,b ... ,c) ↦ `(,a ,@b (S ,c))])
+
+
+; simple transforms -------------------------------------
+
+(define/↦ delete
+  [`(,a ... (S ,b ...) ,c ...) ↦ `(S (,@a ,@c ))])
+
+(define/↦ insert-child-r
+  [`(S (,a ...)) ↦ `(,@a (S (new)))])
+
+(define/↦ insert-child-l
+  [`(S (,a ...)) ↦ `((S (new)) ,@a)])
+
+(define/↦ new-sibling-r
+  [`(,a ... (S (,b ...)) ,c ...) ↦ `(,@a ,@b (S (new)) ,@c)])
+
+(define/↦ new-sibling-l
+  [`(,a ... (S (,b ...)) ,c ...) ↦ `(,@a (S (new)) ,@b ,@c)])
+
+(define/↦ wrap-single
+  [`(S (,a ...)) ↦ `(S ((,@a)))])
+
+
+; -------------------------------------------------------
+
+(module+ test (require rackunit)
+  
+  ; movement
+  (check-equal? (first-child '(S (0 1 2 3)))
+                '((S 0) 1 2 3))
+  (check-equal? (last-child '(S (0 1 2 3)))
+                '(0 1 2 (S 3)))
+  (check-equal? (parent '(0 1 (S 2) 3))
+                '(S (0 1 2 3)))
+  (check-equal? (next-sibling '(0 1 (S 2) 3))
+                '(0 1 2 (S 3)))
+  (check-equal? (prev-sibling '((S 0) 1 2 3))
+                '(0 1 2 (S 3)))
+
+  ; simple
+  (check-equal? (delete '("a" "b" (S "c") "d"))
+                '(S ("a" "b" "d")))
+  (check-equal? (insert-child-r '(S ("a" "b")))
+                '("a" "b" (S (new))))
+  (check-equal? (insert-child-l '(S ("a" "b")))
+                '((S (new)) "a" "b"))
+  (check-equal? (new-sibling-r '("a" (S ("b")) "d"))
+                '("a" "b" (S (new)) "d"))
+  (check-equal? (new-sibling-l '("a" (S ("b")) "d"))
+                '("a" (S (new)) "b" "d"))
+  (check-equal? (wrap-single '(S ("a" "b")))
+                '(S (("a" "b"))))
+
+  ; composition
+  (check-equal? ((compose wrap-single insert-child-l) '(S (a b)))
+                '((S ((new))) a b)))
+
+
+; -------------------------------------------------------
+
+(define original-source '(1 (2 21 22) 3))
+
+(define input-stream '(#\s #\d #\d #\z #\w #\a #\w))
+
+(define source (simple-select original-source))
+
+(loop source input-stream)
+
+
+
+
 ; make function to remove selector from tree
 ; make function to insert selector at provided position
+
+
+
+
+
+
--- tree-rewrites.rkt
+++ tree-rewrites.rkt
@@ -1,35 +1,12 @@
 #lang racket
 
 
-; (struct S (ls)) ; denotes a (singular) selection
-; (struct Ss (ls)) ; denotes a segment selection
+; (struct S (ls)) ; denotes a selection
 ; (struct D (ls)) ; denotes a de-selection
-
-
-; single selection
-
-(define/match (delete expr)
-  [(`(,a ... (S(,d ...)) ,b ...)) `(S (,@a  ,@b ))])
-
-(define/match (insert-child-r expr child)
-  [(`(S (,a ...)) _) `(,@a (S ,child))])
-
-(define/match (insert-child-l expr child)
-  [(`(S (,a ...)) _) `((S ,child) ,@a)])
-
-(define/match (new-sibling-r expr child)
-  [(`(,a ... (S (,b ...)) ,c ...) _) `(,@a ,@b (S (,child)) ,@c)])
-
-(define/match (new-sibling-l expr child)
-  [(`(,a ... (S (,b ...)) ,c ...) _) `(,@a (S (,child)) ,@b ,@c)])
-
-(define/match (wrap-single expr)
-  [(`(S (,a ...))) `(S ((,@a)))])
 
 
 (define/match (push-sibling-r))
 (define/match (push-sibling-l))
-
 
 
 ; segment selection
@@ -43,14 +20,6 @@
 (define/match (barf-l expr))
 
 
-(delete '(0 1 (S(1)) 2))
-(insert-child-r '(S ("a" "b")) "new")
-(insert-child-l '(S ("a" "b")) "new")
-(new-sibling-r '("a" (S ("b")) "d") "c")
-(new-sibling-l '("a" (S ("b")) "d") "c")
-(wrap-single '(S (1 2)))
-
-
 ; multi-selection
 
 (define/match (swap expr)) ; swap 2 selections
@@ -60,24 +29,6 @@
 ; hetero-selection
 
 (define/match (copy/clone)) ; copy one selection to another
-
-
-; selection movement as rewrite
-
-(define/match (parent expr)
-  [(`(,a ... (S (,b ...)) ,c ...)) `(S (,@a ,@b ,@c))])
-
-(define/match (first-child expr)
-  [(`(S (,a ,b ...))) `((S (,a)) ,@b)])
-
-(define/match (last-child expr)
-  [(`(S (,a ... ,b))) `(,@a (S ,b))])
-
-(define/match (next-sibling expr)
-  [(`(,a ... (S ,b) ,c ,d ...)) `(,@a ,b (S ,c) ,@d)])
-
-(define/match (prev-sibling expr)
-  [(`(,a ... ,b (S ,c) ,d ...)) `(,@a (S ,b) ,c ,@d)])
 
 
 ; segment selection movement
