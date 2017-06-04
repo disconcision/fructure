c3d2da49daa68b5acfe12464d192ddcf85dc93be
disconcision <github@disconcision.com>
2017-06-04 16:43:37 -0400

    refactored transform macros

--- transform-engine-test.rkt
+++ transform-engine-test.rkt
@@ -11,6 +11,18 @@
                 (map transform source)
                 source)])]))
 
+(require (rename-in racket (#%app call)))
+(define-syntax #%app
+  (syntax-rules (↦)
+    [[#%app pattern ↦ result] (#%app [pattern ↦ result])]
+    [(#%app [pattern ↦ result] ...) (letrec ([transform (λ (source)
+                                                          (match source
+                                                            [pattern result] ...
+                                                            [_ (if (list? source)
+                                                                   (map transform source)
+                                                                   source)]))])
+                                      transform)]
+    [(#%app f-expr arg-expr ...) (call f-expr arg-expr ...)]))
 
 ; -------------------------------------------------------
 
@@ -41,77 +53,81 @@
 
 ; move selector -----------------------------------------
 
-(define/↦ first-child
+(define first-child
   [`(S (,a ,b ...)) ↦ `((S ,a) ,@b)])
 
-(define/↦ last-child
+(define last-child
   [`(S (,a ... ,b)) ↦ `(,@a (S ,b))])
 
-(define/↦ parent
+(define parent
   [`(,a ... (S ,b ...) ,c ...) ↦ `(S (,@a ,@b ,@c))])
 
-(define/↦ next-sibling
-  [`(,a ... (S ,b) ,c ,d ...) ↦ `(,@a ,b (S ,c) ,@d)]
-  [`(,a ,b ... (S ,c)) ↦ `((S ,a) ,@b ,c)])
+(define next-sibling
+  ([`(,a ... (S ,b) ,c ,d ...) ↦ `(,@a ,b (S ,c) ,@d)]
+   [`(,a ,b ... (S ,c)) ↦ `((S ,a) ,@b ,c)]))
 
-(define/↦ prev-sibling
-  [`(,a ... ,b (S ,c) ,d ...) ↦ `(,@a (S ,b) ,c ,@d)]
-  [`((S ,a) ,b ... ,c) ↦ `(,a ,@b (S ,c))])
+(define prev-sibling
+  ([`(,a ... ,b (S ,c) ,d ...) ↦ `(,@a (S ,b) ,c ,@d)]
+   [`((S ,a) ,b ... ,c) ↦ `(,a ,@b (S ,c))]))
 
 
 ; simple transforms -------------------------------------
 
-(define/↦ delete
+(define delete
   [`(,a ... (S ,b ...) ,c ...) ↦ `(S (,@a ,@c ))])
 
-(define/↦ insert-child-r
+(define insert-child-r
   [`(S (,a ...)) ↦ `(,@a (S (new)))])
 
-(define/↦ insert-child-l
+(define insert-child-l
   [`(S (,a ...)) ↦ `((S (new)) ,@a)])
 
-(define/↦ new-sibling-r
+(define new-sibling-r
   [`(,a ... (S (,b ...)) ,c ...) ↦ `(,@a ,@b (S (new)) ,@c)])
 
-(define/↦ new-sibling-l
+(define new-sibling-l
   [`(,a ... (S (,b ...)) ,c ...) ↦ `(,@a (S (new)) ,@b ,@c)])
 
-(define/↦ wrap
+(define wrap
   [`(S (,a ...)) ↦ `(S ((,@a)))])
 
 
-; untested transforms -----------------------------------
+; secondary transforms -----------------------------------
 
-(define/↦ transform
-  [`pattern ↦ `result])
+(define push-sibling-r
+  ([`(,a ... (S ,b ...) ,c ,d ...) ↦ `(,@a ,c (S ,@b) ,@d)]
+   [`(,a ... (S ,b ...)) ↦ `((S ,@b) ,@a)]))
 
-(define/↦ push-sibling-r
-  [`(,a ... (S ,b ...) ,c ,d ...) ↦ `(,@a ,c (S ,@b) ,@d)]
-  [`(,a ... (S ,b ...)) ↦ `((S ,@b) ,@a)])
+(define push-sibling-l
+  ([`(,a ... ,b (S ,c ...) ,d ...) ↦ `(,@a (S ,@c) ,b ,@d)]
+   [`((S ,a ...) ,b ...) ↦ `(,@b (S ,@a))]))
 
-(define/↦ push-sibling-l
-  [`(,a ... ,b (S ,c ...) ,d ...) ↦ `(,@a (S ,@c) ,b ,@d)]
-  [`((S ,a ...) ,b ...) ↦ `(,@b (S ,@a))])
-
-(define/↦ merge
+(define merge
   [`(S (,a ...) (,b ...)) ↦ `(S (,@a ,@b))])
 
-(merge `(S (1 2) (3 4)))
-
-(define/↦ pop/splice
+(define pop/splice
   [`(,a ... (S (,b ...)) ,c ...) ↦ `(S (,@a ,@b ,@c))])
 
-(define/↦ slurp-r
-  [`(,a ... (S ,b ...) ,c ,d ...) ↦ `(,@a (S ,@b ,c) ,@d)])
+(define slurp-r
+  [`(,a ... (S (,b ...)) ,c ,d ...) ↦ `(,@a (S (,@b ,c)) ,@d)])
 
-(define/↦ slurp-l
-  [`(,a ... ,b (S ,c ...) ,d ...) ↦ `(,@a (S ,b ,@c) ,@d)])
+(define slurp-l
+  [`(,a ... ,b (S (,c ...)) ,d ...) ↦ `(,@a (S (,b ,@c)) ,@d)])
 
-(define/↦ barf-r
-  [`(,a ... (S ,b ... ,c) ,d ...) ↦ `(,@a (S ,@b) ,c ,@d)])
+(define barf-r
+  [`(,a ... (S (,b ... ,c)) ,d ...) ↦ `(,@a (S (,@b)) ,c ,@d)])
 
-(define/↦ barf-l
-  [`(,a ... (S ,b ,c ...) ,d ...) ↦ `(,@a ,b (S ,@c) ,@d)])
+(define barf-l
+  [`(,a ... (S (,b ,c ...)) ,d ...) ↦ `(,@a ,b (S (,@c)) ,@d)])
+
+
+; mathy transforms --------------------------------------
+
+(define comm
+  [`(S (,op ,a ,b)) ↦ `(S (,op ,b ,a))])
+
+(define assoc
+  [`(S (,op ,a (,op ,b ,c))) ↦ `(S (,op (,op ,a ,b) ,c))])
 
 
 ; -------------------------------------------------------
@@ -144,6 +160,24 @@
   (check-equal? (wrap '(S ("a" "b")))
                 '(S (("a" "b"))))
 
+  ; secondary
+  (check-equal? (push-sibling-r '(1 (S 2) 3 4))
+                '(1 3 (S 2) 4))
+  (check-equal? (push-sibling-l '(1 (S 2) 3 4))
+                '((S 2) 1 3 4))
+  (check-equal? (merge '(S (1 2) (3 4)))
+                '(S (1 2 3 4)))
+  (check-equal? (pop/splice '(1 (S (2 21 22)) 4))
+                '(S (1 2 21 22 4)))
+  (check-equal? (slurp-r '((S (1 2)) 3 4))
+                '((S (1 2 3)) 4))  
+  (check-equal? (slurp-l '(1 (S (2 3)) 4))
+                '((S (1 2 3)) 4))
+  (check-equal? (barf-r '((S (1 2 3)) 4))
+                '((S (1 2)) 3 4))  
+  (check-equal? (barf-l '((S (1 2 3)) 4))
+                '(1 (S (2 3)) 4))  
+  
   ; composition
   (check-equal? ((compose wrap insert-child-l) '(S (a b)))
                 '((S ((new))) a b)))
@@ -161,7 +195,7 @@
 
 
 
-
+; app-get override for [ ↦ ] and ([ ↦ ] ... )
 ; make function to remove selector from tree
 ; make function to insert selector at provided position
 ; utility fns
