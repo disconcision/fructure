bb9845fa6be47094416dd0f51ae4023e23ac5c14
disconcision <github@disconcision.com>
2017-06-07 19:15:42 -0400

    style improvements

--- gui-construction-test.rkt
+++ gui-construction-test.rkt
@@ -19,31 +19,34 @@
                     (init-field parent-editor)
                     (init-field position)
                     (field [containing-snip (void)])
-                    
-                    (define/public (format-vertical)
-                      (let ([num-items (send this last-position)])
-                        (for ([pos (range 1 (sub1 (* 2 num-items)) 2)])
-                          (send this insert "\n" pos))))
-                    
-                    (define/public (format-horizontal)
-                      (remove-text-snips))
 
+                    (define/public (get-parent-editor) parent-editor)
+                    (define/public (get-pos) position)
+                    (define/public (set-snip! a-snip) (set! containing-snip a-snip))
+                    (define/public (get-snip) containing-snip)
+                    
                     (define/public (remove-text-snips)
                       (for ([pos (range 0 (send this last-position))])
                         (when (is-a? (send this find-snip pos 'before) string-snip%)
                           (send this release-snip (send this find-snip pos 'before))
-                          (remove-text-snips)) ))
+                          (remove-text-snips))))
 
+                    (define/public (format-horizontal)
+                      (remove-text-snips))
+                    
+                    (define/public (format-vertical)
+                      (remove-text-snips)
+                      (let ([num-items (send this last-position)])
+                        (for ([pos (range 1 (sub1 (* 2 num-items)) 2)])
+                          (send this insert "\n" pos))))
+                    
                     (define/public (format-vertical-fixed-indent-after start-at)
+                      (remove-text-snips)
                       (let ([num-items (send this last-position)])
                         (for ([pos (range start-at (* 2 (sub1 num-items)) 2)])
                           (send this insert "\n" pos))
                         (for ([line-num (range 1 (sub1 num-items))])
                           (send this insert "    " (send this line-start-position line-num)))))
-                    
-                    (define/public (get-parent-editor) parent-editor)
-                    (define/public (get-pos) position)
-                    (define/public (set-snip! a-snip) (set! containing-snip a-snip))
                     
                     ))
 
@@ -52,16 +55,25 @@
                     
                     (init-field parent-editor)
                     (init-field position)
+                    (init-field editor)
 
                     (define children '())
                     (define border-color "MediumVioletRed")
-                    (define background-color (make-color (modulo (* 200 (/ (length position) 5)) 256) 100 150))
-                          
-                    (super-new [with-border? #f])
+                    (define background-color "green" #;(make-color (modulo (* 200 (/ (length position) 5)) 256) 100 150))
                     
-                    (define/public (change-color color)
-                      (set! border-color color))
+                    
+                    
+                    (super-new [with-border? #f] [editor editor])
+                    
+                    (define/public (set-background-color color)
+                      (set! background-color color))
 
+                    (define/public (set-format format)
+                      (match format
+                        ['horizontal (send editor format-horizontal)]
+                        ['vertical (send editor format-vertical)]
+                        ['indent (send editor format-vertical-fixed-indent-after 2)]))
+                    
                     (define/override (draw dc x y left top right bottom dx dy draw-caret)
                       (send dc set-brush background-color 'solid)
                       (send dc set-pen border-color 1 'solid)
@@ -92,28 +104,59 @@
       insert "top")
 
 
-(define my-style-delta (make-object style-delta%))
-(send my-style-delta set-delta-background (make-object color% 100 100 200))
-(send my-style-delta set-transparent-text-backing-on #f)
+(struct block-data (position type style ed sn))
 
 
-(define stylesheet '((default
-                       (background-color "blue")
-                       (format format-horizontal))
-                     (define
-                       (background-color "green")
-                       (format format-vertical-fixed-indent-after 2))))
+#; (define stylesheet '((default
+                          (background-color "blue")
+                          (format format-horizontal))
+                        (define
+                          (background-color "green")
+                          (format format-vertical-fixed-indent-after 2))))
 
-(define (find-style atom)
-  (let ([style (filter (λ (style) (equal? atom (first style))) stylesheet)])
-    (if (empty? style)
-        'default
-        (first (first style)))))
 
-(define (build-style-tree code)
-  (if (list? code)
-      (map build-style-tree code)
-      (find-style code)))
+#; (define (find-style atom)
+     (let ([style (filter (λ (style) (equal? atom (first style))) stylesheet)])
+       (if (empty? style)
+           'default
+           (first (first style)))))
+
+
+#; (define/match (show-style-tree gui-tree)
+     [(`(,(block-data _ _ style _ _) ,ls ...)) 
+      `(,style ,@(map show-style-tree ls))])
+
+
+(define (make-style position code)
+  `(my-style
+    (background-color ,(make-color (modulo (* 200 (/ (length position) 5)) 256)
+                                   222
+                                   130))
+    (format ,(match code
+               [`(let ,inits ,body ...) 'indent]
+               [`(let* ,inits ,body ...) 'indent]
+               [`(if ,ls ...) 'indent]
+               [`(begin ,ls ...) 'indent]
+               [`(define ,ls ...) 'indent]
+               [`(for ,inits ,body ...) 'indent]
+               [_ 'horizontal]))))
+
+
+(define/match (set-style! style sn ed)
+  [(`(,name (background-color ,color)
+            (format ,format)) _ _)
+   (begin (send sn set-background-color color)
+          (send sn set-format format)
+          #;(send sn set-align-top-line #t) ; don't exactly understand this
+          (send sn set-margin 4 4 4 4)
+          #;(send sn set-inset 0 0 0 0) ; ???
+          (send sn use-style-background #t)
+          (begin (define my-style-delta (make-object style-delta%))
+                 (send my-style-delta set-delta-background color)
+                 (send my-style-delta set-alignment-on 'top)
+                 (send my-style-delta set-transparent-text-backing-on #f)
+                 (send ed change-style my-style-delta)))])
+
 
 ; notes on styling
 ; so to start, we decide the style for a list-node based on the first element
@@ -125,19 +168,52 @@
          [sn (new fruct-sn% [editor ed] [parent-editor parent-ed] [position position])])
     (send ed set-snip! sn)
     (send parent-ed insert sn)
-    (send ed change-style my-style-delta)
+    #;(send ed change-style my-style-delta)
     (if (list? code)
         (begin (map (λ (sub pos) (build-gui sub ed (append position `(,pos)))) code (range 0 (length code)))
                #; (insert test stuff here))
         (send ed insert (~v code)))))
 
 
-(define source '(define (format-vertical)
-                  (let ([num-items (send this last-position)])
-                    (for ([pos (range 1 (sub1 (* 2 num-items)) 2)])
-                      (send this insert "\n" pos)))))
+; build gui and return tree with references to relevant objects
+(define (build-gui-block code [parent-ed my-board] [position '()])
+  (let* ([ed (new fruct-ed% [parent-editor parent-ed] [position position])]
+         [sn (new fruct-sn% [editor ed] [parent-editor parent-ed] [position position])]
+         [style (make-style position code)])
+    (send ed set-snip! sn)
+    (send parent-ed insert sn)
+    (if (list? code)
+        (let* ([builder (λ (sub pos) (build-gui-block sub ed (append position `(,pos))))]
+               [kids (map builder code (range 0 (length code)))])
+          (set-style! style sn ed) ; need to set style after children are inserted
+          `(,(block-data position 'list style ed sn) ,@kids))
+        (begin (set-style! style sn ed) ; styler must be first else deletes text 
+               (send ed insert (~v code))
+               ; add case for selector?
+               `(,(block-data position 'atom style ed sn))))))
 
-(build-gui source)
 
-(build-style-tree source)
+(define source '(define (build-gui-block code [parent-ed my-board] [position '()])
+                  (let* ([ed (new fruct-ed% [parent-editor parent-ed] [position position])]
+                         [sn (new fruct-sn% [editor ed] [parent-editor parent-ed] [position position])]
+                         [style (make-style position code)])
+                    (send ed set-snip! sn)
+                    (send parent-ed insert sn)
+                    (if (list? code)
+                        (let* ([builder (λ (sub pos) (build-gui-block sub ed (append position `(,pos))))]
+                               [kids (map builder code (range 0 (length code)))])
+                          (set-style! style sn ed)
+                          `(,(block-data position 'list style ed sn) ,@kids))
+                        (begin (set-style! style sn ed)
+                               (send ed insert (~v code))
+                               `(,(block-data position 'atom style ed sn)))))))
+
+
+(define source-0 '(0 1 (2 21 22 (23 231 232))))
+
+
+(define gui-block (build-gui-block source))
+
+
+
 
