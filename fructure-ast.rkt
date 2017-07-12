c721396dd1431eec882a2ce0499e74b9f4897d06
Andrew Blinn <me@disconcision.com>
2017-07-12 18:36:46 -0400

    basic parse done

--- fructure-ast.rkt
+++ fructure-ast.rkt
@@ -61,9 +61,9 @@
 (define L0-form-names '(if begin define let))
 (define L0-terminal-names '(atom))
 
-(define sort-names '(atom hole expr name literal))
+(define L1-sort-names '(atom hole expr))
 (define L1-form-names '(if begin define let))
-(define L1-terminal-names '(name literal))
+(define L1-terminal-names '(name name-ref literal))
 
 ; L0 : simplest sort model: a single non-atomic sort called 'hole'
 (define lang-expr-holes '(|| (if hole hole hole)
@@ -87,14 +87,14 @@
 ; used in get-pat
 (define lang-expr-xpand '(|| `(if expr expr expr)
                              `(begin ,@(make-list (length as) `expr))
-                             `(define (name ,@(make-list (length bs) `expr)) ,@(make-list (length cs) `expr))
+                             `(define (name ,@(make-list (length bs) `name)) ,@(make-list (length cs) `expr))
                              `(let (,@(make-list (length as) `[name expr])) ,@(make-list (length cs) `expr))))
      
 ; sort-checking patterns
 (define lang-expr-cksrt '(|| (if ,(? (sort: expr) a) ,(? (sort: expr) b) ,(? (sort: expr) c))
                              (begin ,(? (sort: expr) a) ...)
-                             (define (,(? (sort: name) a) ,(? (sort: name) b) ...) ,(? (sort: name) c) ...)
-                             (let ([,(? (sort: name) a) ,(? (sort: name) b)] ...) ,(? (sort: name) c) ...)))
+                             (define (,(? (sort: name) a) ,(? (sort: name) b) ...) ,(? (sort: expr) c) ...)
+                             (let ([,(? (sort: name) a) ,(? (sort: name) b)] ...) ,(? (sort: expr) c) ...)))
 
 
 ; ----------------------------------------------------------------------------
@@ -115,18 +115,17 @@
 ; ----------------------------------------------------------------------------
 
 
-(define test-src '(define (fn a) a))
+(define test-src '(define (fn a) a (define (g q r) 2)))
 
 
-(define (apply-ith i fn source)
-  (lens-transform (list-ref-lens i) source fn))
+
 
 (define atom? (compose not pair?))
 
-(define-match-expander atom
-  (syntax-rules ()
-    [(atom <name>)
-     (? (compose not pair?) <name>)]))
+(define proper-list? (conjoin list? (compose not empty?)))
+
+
+
 
 (require (rename-in racket (#%app call)))
 (define-syntax #%app
@@ -145,20 +144,29 @@
        transform)]
     [(#%app f-expr arg-expr ...) (call f-expr arg-expr ...)]))
 
+
 (define-syntax-rule (map-into source [<pattern> <payload> ...] ...)
-  (letrec
-      ([recursor
-        (match-lambda
-          [<pattern> <payload> ...] ...
-          [x (if (list? x) (map recursor x) x)])])
+  (letrec ([recursor (match-lambda
+                       [<pattern> <payload> ...] ...
+                       [(? list? x) (map recursor x)]
+                       [(? atom? x) x])])
     (recursor source)))
 
-(define (hole-pat pat)
-  (if (atom? pat)
-      (if (member pat sort-names) 'hole pat)
-      (map hole-pat pat)))
+(define-syntax-rule (map-into-fruct source [<pattern> <payload> ...] ...)
+  (letrec ([recursor (match-lambda
+                       [<pattern> <payload> ...] ...
+                       [`(,x . ,xs) (map recursor xs)]
+                       [(? atom? x) x])])
+    (recursor source)))
 
-(define (sel◇ source) `(◇ ,source))
+; ----------------------------------------------------------------------------
+
+#; (define (hole-pat pat)
+     (if (atom? pat)
+         (if (member pat L1-sort-names) 'hole pat)
+         (map hole-pat pat)))
+
+
 
 
 (define (get-pat source)
@@ -168,36 +176,65 @@
     [`(begin ,as ...)
      `(begin ,@(make-list (length as) `expr))]
     [`(define (,a ,bs ...) ,cs ...)
-     `(define (name ,@(make-list (length bs) `expr)) ,@(make-list (length cs) `expr))]
+     `(define (name ,@(make-list (length bs) `name)) ,@(make-list (length cs) `expr))]
     [`(let ([,as ,bs] ...) ,cs ...)
      `(let (,@(make-list (length as) `[name expr])) ,@(make-list (length cs) `expr))]
-    [(atom a)
-     a]))
+    [(? atom? a) 
+     (if (symbol? a) a a #;#;
+         `(name-ref ,a)
+         `(literal ,a))]))
+
+; ----------------------------------------------------------------------------
 
 
-(define (parse source [context 'hole])
-  (println context)
-  (let ([length (when (list? source) (range 0 (length source)))])
-    (match* (source context)
-      [(`(,ls ...) 'hole)
-       (let* ([pat `(◇ ,(hole-pat (get-pat source)))]
-              [contexts (map (λ (i s) (if (or (list? s) (member s L1-form-names))
-                                          ([(◇ (,ls ...)) ⋱↦ ,(apply-ith i (λ (x) `(◇ ,s)) ls)] pat)
-                                          'hole))
-                             length source)]) ; should be pat or source?
-         `(,(hash 'self pat) ,@(map parse source contexts)))]
-      [(`(,ls ...) `(,ls1 ...))
-       (let* ([pat context]
-              [contexts (map (λ (i s) (([(◇ (,ls ...)) ⋱↦ ,(apply-ith i (λ (x) `(◇ ,(if (list? x) x 7))) ls)]) pat)) length source)])
-         `(,(hash 'self context) ,@(map parse source contexts)))]
-      [((atom a) 'hole) (hash 'self (hole-pat (get-pat source)))]
-      [((atom a) _) (hash 'self context)]
-      [(_ _) (println "error on") (println source) (println context)])))
+(define (sel◇ source) `(◇ ,source))
 
+(define/match (get◇ source)
+  [(`(◇ ,a)) a]
+  [((? atom? a)) #f]
+  [(_) (let ([result (filter identity (map get◇ source))])
+         (if (empty? result) #f (first result)))])
+
+(define (apply-ith i fn source)
+  (lens-transform (list-ref-lens i) source fn))
+
+(define (select◇-ith i)
+  [(◇ (,ls ...)) ⋱↦ ,(apply-ith i sel◇ ls)])
+
+(define (get-child-contexts temp src)
+  (map (λ (i) ((select◇-ith i) temp)) (range 0 (length src))))
+
+
+(define (parse source [ctx `(top (◇ expr))])
+  (match (get◇ ctx)
+    [(? (λ (x) (member x L1-terminal-names)))
+     (hash 'self source 'context ctx)]
+    [(? (λ (x) (member x L1-form-names)))
+     (hash 'self source 'context ctx)]
+    ['expr
+     (let ([pat (get-pat source)])
+       (if (list? pat)
+           `(,(hash 'self pat 'context ctx) ,@(map parse source (get-child-contexts `(◇ ,pat) source)))
+           (hash 'self pat 'context ctx)))]
+    [(? list?)
+     `(,(hash 'self '() 'context ctx) ,@(map parse source (get-child-contexts ctx source)))]))
 
 (pretty-print (parse test-src))
 
-(map-into test-src ['a 'b])
+
+(define/match (get-symbol fruct)
+  [(`(,(hash-table ('self s)) ,ls ...)) s])
+
+(define/match (project-symbol fruct)
+  [(`(,x ,xs ...)) (map project-symbol xs)]
+  [((hash-table ('self s))) s])
+
+
+test-src
+(project-symbol (parse test-src))
+(map-into-fruct (parse test-src) [(hash-table ('self s)) s])
+
+#;(map-into test-src ['a 'b])
 
 ; ----------------------------------------------------------------------------
 
@@ -220,7 +257,7 @@
                                [kids (if (empty? xs) xs (map recursor source obj-kids xs))])
                     `(,(fruct sort type name text style mt) ,@kids))] ...
                  [ls `(,(fruct 'unidentified 'unidentified 'unidentified text style mt) ,@(map recursor source obj-kids))])]
-              [`(,(atom sort) ,name ,type)
+              [`(,(? atom? sort) ,name ,type)
                (if (list? source) ; bit of a hack to escape-hatch things unaccounted-for in the form grammar
                    `(,(fruct sort type name text style mt) ,@(map recursor source obj-kids))
                    `(,(fruct sort type name text 0 mt)))]
